25a6ad7bf69180ec63c817b6d7b825f2cd8e3920
Gabriel <gabriel.gsm2017@gmail.com>
2021-12-20 19:23:15 -0300

    at

--- main.js
+++ main.js
@@ -1,66 +1,53 @@
-const valorDolar = 5.67;
+const ehNumero = new RegExp("^(([\\d]{1,7})(\\,([\\d]{0,2}))?)$");
 
-function MascaraMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e){
-    var sep = 0;
-    var key = '';
-    var i = j = 0;
-    var len = len2 = 0;
-    var strCheck = '0123456789';
-    var aux = aux2 = '';
-    var whichCode = (window.Event) ? e.which : e.keyCode;
-    if (whichCode == 13) return true;
-    key = String.fromCharCode(whichCode); // Valor para o código da Chave
-    if (strCheck.indexOf(key) == -1) return false; // Chave inválida
-    len = objTextBox.value.length;
-    for(i = 0; i < len; i++)
-        if ((objTextBox.value.charAt(i) != '0') && (objTextBox.value.charAt(i) != SeparadorDecimal)) break;
-    aux = '';
-    for(; i < len; i++)
-        if (strCheck.indexOf(objTextBox.value.charAt(i))!=-1) aux += objTextBox.value.charAt(i);
-    aux += key;
-    len = aux.length;
-    if (len == 0) objTextBox.value = '';
-    if (len == 1) objTextBox.value = '0'+ SeparadorDecimal + '0' + aux;
-    if (len == 2) objTextBox.value = '0'+ SeparadorDecimal + aux;
-    if (len > 2) {
-        aux2 = '';
-        for (j = 0, i = len - 3; i >= 0; i--) {
-            if (j == 3) {
-                aux2 += SeparadorMilesimo;
-                j = 0;
-            }
-            aux2 += aux.charAt(i);
-            j++;
+const valorAtualDolarEmReais = 5.72;
+let valorDolar = 1;
+let valorReal = 5.72;
+
+function onkeyupDolar(inputDolar) {
+    const convertido = tranformarValor(inputDolar.value, valorDolar);
+
+    const calculo = valorDolar * valorAtualDolarEmReais;
+    const temp = Math.floor(calculo * 100);
+    const resultado = (temp /100).toFixed(2);
+
+    const inputReal = document.getElementById("real");
+    inputReal.value = resultado.toString().replace(".", ",");
+
+    inputDolar.value = convertido;
+}
+
+function onkeyupReal(inputReal) {
+    const convertido = tranformarValor(inputReal.value, valorReal);
+
+    const calculo = valorDolar / valorAtualDolarEmReais;
+    const temp = Math.floor(calculo * 100);
+    const resultado = (temp / 100).toFixed(2);
+
+    const inputDolar = document.getElementById("dolar");
+    inputDolar.value = resultado.toString().replace(".", ",");
+
+    inputReal.value = convertido;
+}
+
+function tranformarValor(valorInput, valorMudanca) {
+    let temVirgula = false;
+
+    if(ehNumero.test(valorInput)){
+        
+        /* quando a diferença entre o length e o indexOf for igual a 1
+        segnifica que o numero posssui uma virgula sem numeros depois.
+        ex: 123,*/
+        if(valorInput.length - valorInput.indexOf(',') === 1){
+            temVirgula = true; 
         }
-        objTextBox.value = '';
-        len2 = aux2.length;
-        for (i = len2 - 1; i >= 0; i--)
-        objTextBox.value += aux2.charAt(i);
-        objTextBox.value += SeparadorDecimal + aux.substr(len - 2, len);
+
+        valorMudanca = parseFloat(valorInput.replace(",", "."));
+        
+    } else if(valorInput === ""){
+        valorMudanca = 0;
     }
-    return false;
+    
+    const convertido = valorMudanca.toString().replace(".", ",");
+    return temVirgula ? `${convertido},` : convertido;;
 }
-
-function inputDolar(valor) {
-
-    const real = document.getElementById('real');
-    const soma = parseFloat(valor.value) * parseFloat(valorDolar);
-
-    const casaDecimal = Math.floor(soma * 100) / 100;
-    const  convertido = isNaN(casaDecimal) ? 0 : casaDecimal.toString().replace(".", ",");
-    real.value = convertido;
-
-}
-
-function inputReal(valorReal) {
-    const dolar = document.getElementById('dolar');
-    const soma = parseFloat(valorReal.value.replaceAll('.', '').replace(",", ".")) / valorDolar;
-
-    const casaDecimal = Math.floor(soma * 100) / 100;
-    const  convertido = isNaN(casaDecimal) ? 0 : casaDecimal.toString().replace(".", ",");
-    dolar.value = convertido;
-}
-
-
-
-
